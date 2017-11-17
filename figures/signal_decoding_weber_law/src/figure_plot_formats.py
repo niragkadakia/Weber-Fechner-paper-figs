1245179dfdb58e59b6df19c8e5ee7784b1b804e3
nk479 <nirag.kadakia@yale.edu>
2017-11-16 21:19:23 -0500

    signal_decoding figure font to Times New Roman

--- figures/signal_decoding_weber_law/src/figure_plot_formats.py
+++ figures/signal_decoding_weber_law/src/figure_plot_formats.py
@@ -15,9 +15,10 @@ from matplotlib import cm
 from matplotlib import rc
 import matplotlib.pyplot as plt
 plt.rcParams['text.latex.preamble'] = [
-		r'\usepackage{helvet}',
-		r'\usepackage{sansmath}', # math --> arial
-		r'\sansmath']             # <-- actually tell tex to use it!
+		r'\usepackage{helvet}', 
+		r'\usepackage{sansmath}', 
+		r'\sansmath']   
+seriffont={'fontname':'Times New Roman'}
 import matplotlib.gridspec as gridspec
 from mpl_toolkits.mplot3d import Axes3D
 from local_methods import def_data_dir
@@ -44,7 +45,7 @@ def single_decoding_weber_law_plot(data_idxs=2):
 	num_gain_plots = data_idxs/2
 	gain_plot_size = 4
 	est_plot_upper_pad = 3
-	est_plot_height = 4
+	est_plot_height = 6
 	num_est_plots = 2
 	
 	
@@ -53,15 +54,15 @@ def single_decoding_weber_law_plot(data_idxs=2):
 	grid_height = decode_plot_size + est_plot_upper_pad + \
 					est_plot_height*num_est_plots
 	gs = gridspec.GridSpec(grid_height, grid_width)
-	gs.update(wspace=6.5, hspace=3.0)
-	fig.set_size_inches(13, 12)
+	gs.update(wspace=6.5, hspace=1.0)
+	fig.set_size_inches(13, 16)
 	
 	# Some font and label sizes for later use
-	tick_label_size_small = 20
-	tick_label_size_large = 22
-	axis_label_size = 28
-	x_axis_label_pad = 13
-	y_axis_successes_labelpad = 10
+	tick_label_size_small = 23
+	tick_label_size_large = 25
+	axis_label_size = 34
+	x_axis_label_pad = 15
+	y_axis_successes_labelpad = 8
 	x_tick_label_pad = 11
 	
 	ax = dict()
@@ -69,16 +70,16 @@ def single_decoding_weber_law_plot(data_idxs=2):
 	# Success error plots
 	ax['successes'] = plt.subplot(gs[0:decode_plot_size, 0:decode_plot_size])
 	plt.xscale('log')
-	plt.xticks(10.**sp.arange(-5, 5),  fontsize=tick_label_size_small)
+	plt.xticks(10.**sp.arange(-5, 5),  fontsize=tick_label_size_large)
 	plt.yticks([0, 50, 100], fontsize=tick_label_size_large)
 	ax['successes'].tick_params(axis='x', which='major', pad=x_tick_label_pad)
 	ax['successes'].set_xlabel(r'Background signal', fontsize=axis_label_size, 
-								labelpad=x_axis_label_pad)
-	ax['successes'].set_ylabel(r'Correctly decoded''\n''signals ($\%$)', 
+								labelpad=x_axis_label_pad, **seriffont)
+	ax['successes'].set_ylabel(r'Correctly decoded (%)', 
 								fontsize=axis_label_size, 
 								labelpad=y_axis_successes_labelpad, 
-								multialignment='center')
-	ax['successes'].set_xlim([1e-1, 1e2])
+								multialignment='center', **seriffont)
+	ax['successes'].set_xlim([1e-1, 1.5e2])
 	
 	# Gain plots
 	for data_idx in sp.arange(0, data_idxs):
@@ -104,33 +105,36 @@ def single_decoding_weber_law_plot(data_idxs=2):
 			ax['gains_%s' % data_idx].set_yticks(10.**sp.arange(-5, 0))
 			ax['gains_%s' % data_idx].tick_params(labelsize=tick_label_size_large)
 		if data_idx % 2 == 0:
-			ax['gains_%s' % data_idx].set_ylim(3e-3, 4e-2)
+			ax['gains_%s' % data_idx].set_ylim(2e-3, 4e-2)
 		elif data_idx % 2 == 1:
-			ax['gains_%s' % data_idx].set_ylim(1e-2, 2e-1)
+			ax['gains_%s' % data_idx].set_ylim(1.1e-2, 2e-1)
 			ax['gains_%s' % data_idx].set_xticks([1e-2, 1e-1, 1e0])	
-			ax['gains_%s' % data_idx].tick_params(labelsize=tick_label_size_small)
-		ax['gains_%s' % data_idx].set_xlim(0.98e-2, 1.1e-1)
+			ax['gains_%s' % data_idx].tick_params(axis='x', labelsize=tick_label_size_small)
+			ax['gains_%s' % data_idx].tick_params(axis='y', labelsize=tick_label_size_large)
+		ax['gains_%s' % data_idx].set_xlim(0.98e-2, 1.4e-1)
 		ax['gains_%s' % data_idx].tick_params(axis='x', which='major', 
 												pad=x_tick_label_pad)
 			
 		# Gaain axis label in center of range of plots
 		if num_gain_plots % 2 == 0:
 			if data_idx == data_idxs/2 - 1:
-				ax['gains_%s' % data_idx].xaxis.set_label_coords(1.1, -0.38)
+				ax['gains_%s' % data_idx].xaxis.set_label_coords(1.1, -0.36)
 				ax['gains_%s' % data_idx].set_xlabel(r'Background signal', 
 											fontsize=axis_label_size, 
-											labelpad=x_axis_label_pad)
+											labelpad=x_axis_label_pad, 
+											 **seriffont)
 		elif num_gain_plots % 2 == 1:
 			if data_idx == data_idxs/2:
 				ax['gains_%s' % data_idx].set_xlabel(r'Background signal', 
 											fontsize=axis_label_size, 
-											labelpad=x_axis_label_pad)
+											labelpad=x_axis_label_pad, 
+											 **seriffont)
 		if data_idx == data_idxs - 2:
 			ax['gains_%s' % data_idx].yaxis.set_label_position('right')
 			ax['gains_%s' % data_idx].set_ylabel(
-						r'Receptor gain''\n''$\Delta a / \Delta s$', 
-						fontsize=axis_label_size, rotation=270)
-			ax['gains_%s' % data_idx].yaxis.set_label_coords(1.50, -0.1)
+						r'Receptor gain', fontsize=axis_label_size, 
+						rotation=270, **seriffont)
+			ax['gains_%s' % data_idx].yaxis.set_label_coords(1.4, -0.1)
 				
 	# Sample estimation plot
 	for est_idx in range(num_est_plots):
@@ -143,13 +147,15 @@ def single_decoding_weber_law_plot(data_idxs=2):
 		
 		# Sample estimation plots labels only on outside
 		if est_idx == num_est_plots/2:
-			ax['est_%s' % est_idx].set_xticks(sp.arange(0, 100, 10))
+			ax['est_%s' % est_idx].set_xticks(sp.arange(10, 100, 20))
 			ax['est_%s' % est_idx].yaxis.set_label_coords(-0.03, 1.1)
 			ax['est_%s' % est_idx].set_xlabel(r'Odorant identity', 
 												fontsize=axis_label_size,
-												labelpad=x_axis_label_pad)
+												labelpad=x_axis_label_pad, 
+												 **seriffont)
 			ax['est_%s' % est_idx].set_ylabel(r'Odorant intensity', 
-												fontsize=axis_label_size) 
+												fontsize=axis_label_size, 
+												 **seriffont)
 			ax['est_%s' % est_idx].tick_params(labelsize=tick_label_size_large, 
 												pad=x_tick_label_pad)
 		else:
