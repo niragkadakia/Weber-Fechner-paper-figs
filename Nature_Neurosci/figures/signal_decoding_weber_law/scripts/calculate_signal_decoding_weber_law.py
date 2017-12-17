a0a828b6b81b9a01b42c59c0ea82f10bf42417f9
nk479 <nirag.kadakia@yale.edu>
2017-12-17 12:15:38 -0500

    Accept multiple dataflags in calculation scripts

--- Nature_Neurosci/figures/signal_decoding_weber_law/scripts/calculate_signal_decoding_weber_law.py
+++ Nature_Neurosci/figures/signal_decoding_weber_law/scripts/calculate_signal_decoding_weber_law.py
@@ -20,57 +20,62 @@ from save_load_data import load_aggregated_object_list, \
 							save_signal_decoding_weber_law
 
 
-def calculate_signal_decoding_weber_law(data_flag, 
+def calculate_signal_decoding_weber_law(data_flags, 
 										nonzero_bounds=[0.5, 1.5], 
 										zero_bound=1./10., 
 										threshold_pct_nonzero=85.0, 
 										threshold_pct_zero=85.0):
+	"""
+	"""
 	
-	list_dict = read_specs_file(data_flag)
-	for key in list_dict:
-		exec("%s = list_dict[key]" % key)
+	for data_flag in data_flags:
 
-	iter_vars_dims = []
-	for iter_var in iter_vars:
-		iter_vars_dims.append(len(iter_vars[iter_var]))		
-	
-	print ('Loading object list...'),
-	CS_object_array = load_aggregated_object_list(iter_vars_dims, data_flag)
-	print ('...loaded.')
+		list_dict = read_specs_file(data_flag)
+		for key in list_dict:
+			exec("%s = list_dict[key]" % key)
 
-	# Data structures 
-	errors_nonzero = sp.zeros(iter_vars_dims)
-	errors_zero = sp.zeros(iter_vars_dims)
-	epsilons = sp.zeros((iter_vars_dims[0], iter_vars_dims[1], params['Mm']))
-	gains = sp.zeros((iter_vars_dims[0], iter_vars_dims[1], 
-						params['Mm'], params['Nn']))
-	successes = sp.zeros(iter_vars_dims)
-	
-	# Calculate binary errors
-	it = sp.nditer(sp.zeros(iter_vars_dims), flags=['multi_index'])	
-	while not it.finished:
-		errors = binary_errors(CS_object_array[it.multi_index], 
-								nonzero_bounds=nonzero_bounds,
-								zero_bound=zero_bound)
+		iter_vars_dims = []
+		for iter_var in iter_vars:
+			iter_vars_dims.append(len(iter_vars[iter_var]))		
 		
-		errors_nonzero[it.multi_index] = errors['errors_nonzero']
-		errors_zero[it.multi_index] = errors['errors_zero']
-		it.iternext()
-	
-	# Calculate success ratios from binary errors
-	it = sp.nditer(sp.zeros(iter_vars_dims), flags = ['multi_index'])
-	while not it.finished:
-		successes[it.multi_index] = binary_success(
-					errors_nonzero[it.multi_index], 
-					errors_zero[it.multi_index], 
-					threshold_pct_nonzero=threshold_pct_nonzero,
-					threshold_pct_zero=threshold_pct_zero)
-		epsilons[it.multi_index] = CS_object_array[it.multi_index].eps
-		gains[it.multi_index] = CS_object_array[it.multi_index].Rr
-		it.iternext()
-	
-	save_signal_decoding_weber_law(successes, gains, epsilons, data_flag)
-	
+		print ('Loading object list...'),
+		CS_object_array = load_aggregated_object_list(iter_vars_dims, data_flag)
+		print ('...loaded.')
+
+		# Data structures 
+		errors_nonzero = sp.zeros(iter_vars_dims)
+		errors_zero = sp.zeros(iter_vars_dims)
+		epsilons = sp.zeros((iter_vars_dims[0], iter_vars_dims[1], 
+								params['Mm']))
+		gains = sp.zeros((iter_vars_dims[0], iter_vars_dims[1], 
+							params['Mm'], params['Nn']))
+		successes = sp.zeros(iter_vars_dims)
+		
+		# Calculate binary errors
+		it = sp.nditer(sp.zeros(iter_vars_dims), flags=['multi_index'])	
+		while not it.finished:
+			errors = binary_errors(CS_object_array[it.multi_index], 
+									nonzero_bounds=nonzero_bounds,
+									zero_bound=zero_bound)
+			
+			errors_nonzero[it.multi_index] = errors['errors_nonzero']
+			errors_zero[it.multi_index] = errors['errors_zero']
+			it.iternext()
+		
+		# Calculate success ratios from binary errors
+		it = sp.nditer(sp.zeros(iter_vars_dims), flags = ['multi_index'])
+		while not it.finished:
+			successes[it.multi_index] = binary_success(
+						errors_nonzero[it.multi_index], 
+						errors_zero[it.multi_index], 
+						threshold_pct_nonzero=threshold_pct_nonzero,
+						threshold_pct_zero=threshold_pct_zero)
+			epsilons[it.multi_index] = CS_object_array[it.multi_index].eps
+			gains[it.multi_index] = CS_object_array[it.multi_index].Rr
+			it.iternext()
+		
+		save_signal_decoding_weber_law(successes, gains, epsilons, data_flag)
+		
 if __name__ == '__main__':
-	data_flag = get_flag()
-	calculate_signal_decoding_weber_law(data_flag)
+	data_flags = sys.argv[1:]
+	calculate_signal_decoding_weber_law(data_flags)
--- Nature_Neurosci/figures/signal_discrimination_weber_law/scripts/calculate_signal_discrimination_weber_law.py
+++ Nature_Neurosci/figures/signal_discrimination_weber_law/scripts/calculate_signal_discrimination_weber_law.py
@@ -20,63 +20,67 @@ from save_load_data import load_aggregated_object_list, \
 							save_signal_discrimination_weber_law
 
 
-def calculate_signal_discrimination_weber_law(data_flag, 
+def calculate_signal_discrimination_weber_law(data_flags, 
 										nonzero_bounds=[0.5, 1.5], 
 										zero_bound=1./10., 
 										threshold_pct_nonzero=75.0, 
 										threshold_pct_zero=75.0):
+	"""
+	"""
 	
-	list_dict = read_specs_file(data_flag)
-	for key in list_dict:
-		exec("%s = list_dict[key]" % key)
+	for data_flag in data_flags:
+	
+		list_dict = read_specs_file(data_flag)
+		for key in list_dict:
+			exec("%s = list_dict[key]" % key)
 
-	iter_vars_dims = []
-	for iter_var in iter_vars:
-		iter_vars_dims.append(len(iter_vars[iter_var]))		
-	
-	print ('Loading object list...'),
-	CS_object_array = load_aggregated_object_list(iter_vars_dims, data_flag)
-	print ('...loaded.')
+		iter_vars_dims = []
+		for iter_var in iter_vars:
+			iter_vars_dims.append(len(iter_vars[iter_var]))		
+		
+		print ('Loading object list...'),
+		CS_object_array = load_aggregated_object_list(iter_vars_dims, data_flag)
+		print ('...loaded.')
 
-	#Code not written for Kk_split = 0
-	assert CS_object_array[0, 0].Kk_split != 0, "Need nonzero Kk_split"
-	
-	# Data structures 
-	errors_zero = sp.zeros(iter_vars_dims)
-	errors_nonzero_2 = sp.zeros(iter_vars_dims)
-	errors_nonzero = sp.zeros(iter_vars_dims)
-	successes_2 = sp.zeros(iter_vars_dims)
-	successes = sp.zeros(iter_vars_dims)
-	
-	# Calculate binary errors
-	it = sp.nditer(sp.zeros(iter_vars_dims), flags=['multi_index'])	
-	while not it.finished:
-		errors = binary_errors_dual_odor(CS_object_array[it.multi_index], 
-								nonzero_bounds=nonzero_bounds,
-								zero_bound=zero_bound)
+		#Code not written for Kk_split = 0
+		assert CS_object_array[0, 0].Kk_split != 0, "Need nonzero Kk_split"
 		
-		errors_nonzero[it.multi_index] = errors['errors_nonzero']
-		errors_nonzero_2[it.multi_index] = errors['errors_nonzero_2']
-		errors_zero[it.multi_index] = errors['errors_zero']
-		it.iternext()
-	
-	# Calculate success ratios from binary errors
-	it = sp.nditer(sp.zeros(iter_vars_dims), flags = ['multi_index'])
-	while not it.finished:
-		successes_2[it.multi_index] = binary_success(
-					errors_nonzero_2[it.multi_index], 
-					errors_zero[it.multi_index], 
-					threshold_pct_nonzero=threshold_pct_nonzero,
-					threshold_pct_zero=threshold_pct_zero)
-		successes[it.multi_index] = binary_success(
-					errors_nonzero[it.multi_index], 
-					errors_zero[it.multi_index], 
-					threshold_pct_nonzero=threshold_pct_nonzero,
-					threshold_pct_zero=threshold_pct_zero)
-		it.iternext()
+		# Data structures 
+		errors_zero = sp.zeros(iter_vars_dims)
+		errors_nonzero_2 = sp.zeros(iter_vars_dims)
+		errors_nonzero = sp.zeros(iter_vars_dims)
+		successes_2 = sp.zeros(iter_vars_dims)
+		successes = sp.zeros(iter_vars_dims)
 		
-	save_signal_discrimination_weber_law(successes, successes_2, data_flag)
+		# Calculate binary errors
+		it = sp.nditer(sp.zeros(iter_vars_dims), flags=['multi_index'])	
+		while not it.finished:
+			errors = binary_errors_dual_odor(CS_object_array[it.multi_index], 
+									nonzero_bounds=nonzero_bounds,
+									zero_bound=zero_bound)
+			
+			errors_nonzero[it.multi_index] = errors['errors_nonzero']
+			errors_nonzero_2[it.multi_index] = errors['errors_nonzero_2']
+			errors_zero[it.multi_index] = errors['errors_zero']
+			it.iternext()
+		
+		# Calculate success ratios from binary errors
+		it = sp.nditer(sp.zeros(iter_vars_dims), flags = ['multi_index'])
+		while not it.finished:
+			successes_2[it.multi_index] = binary_success(
+						errors_nonzero_2[it.multi_index], 
+						errors_zero[it.multi_index], 
+						threshold_pct_nonzero=threshold_pct_nonzero,
+						threshold_pct_zero=threshold_pct_zero)
+			successes[it.multi_index] = binary_success(
+						errors_nonzero[it.multi_index], 
+						errors_zero[it.multi_index], 
+						threshold_pct_nonzero=threshold_pct_nonzero,
+						threshold_pct_zero=threshold_pct_zero)
+			it.iternext()
+			
+		save_signal_discrimination_weber_law(successes, successes_2, data_flag)
 	
 if __name__ == '__main__':
-	data_flag = get_flag()
-	calculate_signal_discrimination_weber_law(data_flag)
+	data_flags = sys.argv[1:]
+	calculate_signal_discrimination_weber_law(data_flags)
